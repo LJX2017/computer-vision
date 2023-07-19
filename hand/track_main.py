7846f5b3e64e613910bfe3032de5c1628645c42b
xwinp <139521361+xwinp@users.noreply.github.com>
2023-07-19 15:24:45 +0800

    Add files via upload
    hand_tracker

---
+++ hand/track_main.py
@@ -0,0 +1,23 @@
+import cv2
+import mediapipe as mp
+import time
+import track_model
+cap=cv2.VideoCapture(0)
+
+Ptime=0
+Ctime=0
+detector=track_model.handdetector()
+while True:
+    success,img =cap.read()
+    img=detector.findHands(img) 
+    lmList=detector.findposition(img)
+    if(len(lmList)!=0):
+        print(lmList[4])
+    Ctime =time.time()
+    fps=1/(Ctime-Ptime)
+    Ptime=Ctime
+
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3 )
+
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
